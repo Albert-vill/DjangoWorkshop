74ea2e4fe25825eb8637d8abb4bc92b617746a7a
Navega <navega@appfluence.com>
2021-07-07 23:22:16 +0200

    Cookiecutter example.

---
+++ celtiberian/celtiberian/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from celtiberian.users.models import User
+from celtiberian.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
